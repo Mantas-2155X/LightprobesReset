d7ad925705f63deb86690f3c9de293da3a405020
Mantas-2155X <syenergyy@gmail.com>
2020-07-05 22:40:17 +0300

    * implemented extra code given by hooh

--- AI_LightprobesReset/AI_LightprobesReset.cs
+++ AI_LightprobesReset/AI_LightprobesReset.cs
@@ -1,18 +1,24 @@
-﻿using BepInEx;
-using BepInEx.Harmony;
+﻿using System.Collections.Generic;
+
+using BepInEx;
 using HarmonyLib;
+
+using AIChara;
+
 using UnityEngine;
+using UnityEngine.Rendering;
 
 namespace AI_LightprobesReset
 {
     [BepInPlugin(nameof(AI_LightprobesReset), nameof(AI_LightprobesReset), VERSION)][BepInProcess("StudioNEOV2")]
     public class AI_LightprobesReset : BaseUnityPlugin
     {
-        public const string VERSION = "1.0.0";
+        public const string VERSION = "1.1.0";
 
         private void Awake()
         {
-            HarmonyWrapper.PatchAll(typeof(AI_LightprobesReset));
+            var harmony = new Harmony(nameof(AI_LightprobesReset));
+            harmony.PatchAll(typeof(AI_LightprobesReset));
         }
         
         [HarmonyPrefix, HarmonyPatch(typeof(Studio.Map), "LoadMap")]
@@ -26,5 +32,62 @@ namespace AI_LightprobesReset
         {
             LightmapSettings.lightProbes = null;
         }
+        
+        [HarmonyPostfix, HarmonyPatch(typeof(Studio.Studio), nameof(Studio.Studio.AddFemale))]
+        public static void AddFemale() 
+        {
+            FixProbe.SetupProbes();
+        }
+        
+        [HarmonyPostfix, HarmonyPatch(typeof(Studio.Studio), nameof(Studio.Studio.AddMale))]
+        public static void AddMale() 
+        {
+            FixProbe.SetupProbes();
+        }
+        
+        [HarmonyPostfix, HarmonyPatch(typeof(Studio.Studio), nameof(Studio.Studio.LoadScene))]
+        public static void LoadScene() 
+        {
+            FixProbe.SetupProbes();
+        }
     }
+    
+    public static class FixProbe 
+    {
+        private static Transform FindDeepChildPlease(this Transform aParent, string aName) 
+        {
+            var queue = new Queue<Transform>();
+            queue.Enqueue(aParent);
+            
+            while (queue.Count > 0) 
+            {
+                var c = queue.Dequeue();
+                
+                if (c.name == aName)
+                    return c;
+                foreach (Transform t in c)
+                    queue.Enqueue(t);
+            }
+            
+            return null;
+        }
+        public static void SetupProbes() 
+        {
+            var charas = Object.FindObjectsOfType<ChaControl>();
+            foreach (var cha in charas)
+                SetupProbes(cha);
+        }
+        
+        private static void SetupProbes(ChaControl chara) 
+        {
+            var t = FindDeepChildPlease(chara.transform, "cf_J_Hips");
+            foreach (var renderer in chara.GetComponentsInChildren<Renderer>(true)) 
+            {
+                renderer.reflectionProbeUsage = ReflectionProbeUsage.BlendProbes;
+                renderer.lightProbeUsage = LightProbeUsage.BlendProbes;
+                renderer.probeAnchor = t;
+            }
+        }
+    }
+
 }
